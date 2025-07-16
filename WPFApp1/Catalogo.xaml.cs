b9bb30f8cff11827331517ea7d51ed08d96b94a4
Camilo S. <salinas217@outlook.es>
2025-07-15 22:33:01 -0300

    Correción de evento doble click en vista tabular de 'Catalogo'.

--- WPFApp1/Catalogo.xaml.cs
+++ WPFApp1/Catalogo.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace WPFApp1
+﻿using System.Windows.Controls;
+
+namespace WPFApp1
 {
     /// <summary>
     /// Lógica de interacción para Catalogo.xaml
@@ -11,6 +13,20 @@
             DataContext = viewModel;
             _viewModel = viewModel;
             InitializeComponent();
+        }
+        private void Row_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is DataGridRow row && row.Item != null)
+            {
+                var producto = row.Item;
+                if (DataContext is CatalogoViewModel viewModel)
+                {
+                    if (viewModel.ItemDoubleClickCommand != null && viewModel.ItemDoubleClickCommand.CanExecute(producto))
+                    {
+                        viewModel.ItemDoubleClickCommand.Execute(producto);
+                    }
+                }
+            }
         }
     }
 }
