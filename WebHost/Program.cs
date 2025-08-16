192c9f00fc035280eaaa2555b82a9b853469fc6e
Camilo <Salinas217@outlook.es>
2025-08-16 12:28:02 -0300

    feat: Añadir esqueleto del servidor web y front-end básico

--- WebHost/Program.cs
+++ WebHost/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
+app.UseCors("AllowAnyOrigin");
+app.UseStaticFiles();
 
 app.MapGet("/", () => "Hola mundo!");
+
+app.MapPost("/api/subir-imagen", async (IFormFile imagen) => {
+    var nombreArchivo = imagen.FileName;
+    var tamanoArchivo = imagen.Length;
+
+    return Results.Ok(new { Mensaje = "Archivo recibido con éxito", Nombre = nombreArchivo, Tamano = tamanoArchivo });
+});
 
 app.Run();
---
+++ WebHost/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿/**const boton = document.getElementById('Boton1');
+
+boton.addEventListener('click', () => {
+    alert('Botón presionado!');
+});*/
+
+const formulario = document.getElementById('formularioSubirImagen');
+
+formulario.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(formulario);
+
+    try {
+        const respuesta = await fetch('/api/subir-imagen', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (respuesta.ok) {
+            alert('Archivo enviado con éxito');
+        } else {
+            alert('Hubo un error al enviar el archivo.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Ocurrió un error en la conexión.');
+    }
+});
