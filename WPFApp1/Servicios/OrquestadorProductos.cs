5ced31fcb4b16f0d8863df6665d4b020c5abc9e9
Camilo <Salinas217@outlook.es>
2025-09-24 12:30:05 -0300

    Implementación en proceso de Servicio Orquestador.

--- WPFApp1/Servicios/OrquestadorProductos.cs
+++ WPFApp1/Servicios/OrquestadorProductos.cs
@@ -3,8 +3,20 @@ using WPFApp1.Interfaces;
 
 namespace WPFApp1.Servicios
 {
+    public enum ServicioAsociado
+    {
+        Stock,
+        Ubicaciones,
+        Formatos,
+        Versiones,
+        Marcas,
+        Categorias,
+        Indexacion,
+        Producto
+    }
     public class OrquestadorProductos
     {
+        public readonly Dictionary<string, ServicioAsociado> MapeoPropiedades;
         private readonly IProductosServicio productoServicio;
         private readonly ServicioIndexacionProductos indexacionServicio;
         private readonly IConmutadorEntidadGenerica<Formatos> formatoServicio;
@@ -20,33 +32,51 @@ namespace WPFApp1.Servicios
             indexacionServicio = _servicioIndexacion;
             formatoServicio = _servicioFormatos;
             versionesServicio = _servicioVersiones;
+
+            MapeoPropiedades = new Dictionary<string, ServicioAsociado>(StringComparer.OrdinalIgnoreCase)
+            {
+                //Propiedad de Clase , Servicio Asociado
+                {"RutaImagen", ServicioAsociado.Versiones },
+                {"EAN", ServicioAsociado.Versiones },
+                {"MarcaID", ServicioAsociado.Versiones },
+                {"FormatoID", ServicioAsociado.Versiones },
+
+                {"MarcaNombre", ServicioAsociado.Marcas },
+
+                {"UbicacionID", ServicioAsociado.Stock},
+                {"ID", ServicioAsociado.Producto},
+                {"Haber", ServicioAsociado.Stock},
+                {"Precio", ServicioAsociado.Stock},
+                {"EsEliminado", ServicioAsociado.Stock },
+                {"FechaModificacion", ServicioAsociado.Stock},
+                {"FechaCreacion",ServicioAsociado.Stock }
+            };
         }
 
-        public bool EditarProducto(ProductoCatalogo productoModificado)
+        public bool ModificarProducto(ProductoCatalogo productoModificado)
         {
             ProductoCatalogo productoActual = productoServicio.RecuperarProductoPorID(productoModificado.ProductoSKU);
             var propiedadesEntidad = typeof(ProductoCatalogo).GetProperties();
-            var listaBlanca = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            {
-                "UbicacionID",
-                "MarcaID",
-                "FormatoID",
-                "ProductoVersionID"
-            };
 
             foreach (var propiedad in propiedadesEntidad)
             {
-                if (listaBlanca.Contains(propiedad.Name))
+                var valorActual = propiedad.GetValue(productoActual);
+                var valorModificado = propiedad.GetValue(productoModificado);
+                if (!object.Equals(valorActual, valorModificado))
                 {
-                    var valorActual = propiedad.GetValue(productoActual);
-                    var valorModificado = propiedad.GetValue(productoModificado);
-                    if (!object.Equals(valorActual, valorModificado))
-                    {
 
-                    }
                 }
             }
+        }
 
+        public bool ModificarVersion(ProductoCatalogo productoModificado)
+        {
+            Versiones versionActual = versionesServicio.Recuperar(productoModificado.ProductoVersionID);
+            Versiones versionModificada = new Versiones
+            {
+                EAN = productoModificado.EAN,
+
+            }
         }
     }
 }
