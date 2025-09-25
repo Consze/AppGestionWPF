73ea0be54cedc3ca714de5bae92c12f3ad89654f
Camilo <Salinas217@outlook.es>
2025-09-25 12:25:53 -0300

    Implementacion de metodos para servicio Orquestador de productos.

--- WPFApp1/Servicios/OrquestadorProductos.cs
+++ WPFApp1/Servicios/OrquestadorProductos.cs
@@ -12,7 +12,8 @@ namespace WPFApp1.Servicios
         Marcas,
         Categorias,
         Indexacion,
-        Producto
+        Producto,
+        Null
     }
     public class OrquestadorProductos
     {
@@ -48,8 +49,8 @@ namespace WPFApp1.Servicios
                 {"Haber", ServicioAsociado.Stock},
                 {"Precio", ServicioAsociado.Stock},
                 {"EsEliminado", ServicioAsociado.Stock },
-                {"FechaModificacion", ServicioAsociado.Stock},
-                {"FechaCreacion",ServicioAsociado.Stock }
+                {"FechaModificacion", ServicioAsociado.Stock}
+                //{"FechaCreacion",ServicioAsociado.Stock }
             };
         }
 
@@ -58,25 +59,62 @@ namespace WPFApp1.Servicios
             ProductoCatalogo productoActual = productoServicio.RecuperarProductoPorID(productoModificado.ProductoSKU);
             var propiedadesEntidad = typeof(ProductoCatalogo).GetProperties();
 
+            //Bandera
+            bool ActualizarVersion = false;
+
+            //1 - Bucle de relevamiento
             foreach (var propiedad in propiedadesEntidad)
             {
                 var valorActual = propiedad.GetValue(productoActual);
                 var valorModificado = propiedad.GetValue(productoModificado);
                 if (!object.Equals(valorActual, valorModificado))
                 {
-
+                    switch (MapeoPropiedades[propiedad.Name])
+                    {
+                        case ServicioAsociado.Versiones:
+                            ActualizarVersion = true;
+                            break;
+                    }
                 }
             }
+
+            //2- Llamada a servicios condicional
+            if (ActualizarVersion)
+            {
+                string nuevaVersionID = ModificarVersion(productoModificado);
+                productoModificado.ProductoVersionID = nuevaVersionID;
+            }
+
+
+            //3 - Llamada a conmutador para asentar cambios en Stock
+            return productoServicio.ModificarProducto(productoModificado);
         }
 
-        public bool ModificarVersion(ProductoCatalogo productoModificado)
+        public string ModificarVersion(ProductoCatalogo productoModificado)
         {
+            string IDNuevaVersion = string.Empty;
             Versiones versionActual = versionesServicio.Recuperar(productoModificado.ProductoVersionID);
             Versiones versionModificada = new Versiones
             {
                 EAN = productoModificado.EAN,
+                ProductoID = productoModificado.ID,
+                RutaRelativaImagen = productoModificado.RutaImagen,
+                FormatoID = productoModificado.FormatoProductoID,
+                MarcaID = productoModificado.MarcaID,
 
+                /**
+                EsEliminado = Falso por defecto en DB
+                FechaCreacion = DateTime.Now, Completado por conmutador
+                FechaModificacion = Vacio
+                ID = UUID Generada por conmutador
+                */
+            };
+
+            if (!object.Equals(versionActual, versionModificada))
+            {
+                IDNuevaVersion = versionesServicio.Insertar(versionModificada);
             }
+            return IDNuevaVersion;
         }
     }
 }
