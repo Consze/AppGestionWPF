d2d4477a4502e204239b8156a18ef203fef382b3
Camilo S. <salinas217@outlook.es>
2025-11-07 00:46:02 -0300

    Correciones menores de UI para busquedas recientes en vista de Catalogo.

--- WPFApp1/ViewModels/CatalogoViewModel.cs
+++ WPFApp1/ViewModels/CatalogoViewModel.cs
@@ -558,7 +558,6 @@ namespace WPFApp1.ViewModels
                             BusquedasRecientes.RemoveAt(BusquedasRecientes.Count - 1);
                         }
                     }
-                    TextoBusqueda = string.Empty;
                 }
             }
 
